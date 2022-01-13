a340760926f15f738e4587e2dd2c1a944273d654
Ben Fidge <ben.fidge@btopenworld.com>
2022-01-13 15:18:45 +0700

    fix: Language changes

--- src/ExternalSearch.Providers.Bregg/Constants.cs
+++ src/ExternalSearch.Providers.Bregg/Constants.cs
@@ -15,22 +15,12 @@ namespace CluedIn.ExternalSearch.Providers.Bregg
         public static string Icon { get; set; } = "Resources.brreg_logo.svg";
         public static string Domain { get; set; } = "https://www.brreg.no/";
 
-        public static AuthMethods AuthMethods { get; set; } = new()
+        public static AuthMethods AuthMethods { get; set; } = new AuthMethods()
         {
             token = new List<Control>()
         };
 
-        public static IEnumerable<Control> Properties { get; set; } = new List<Control>()
-        {
-            // NOTE: Leaving this commented as an example - BF
-            //new()
-            //{
-            //    displayName = "Some Data",
-            //    type = "input",
-            //    isRequired = true,
-            //    name = "someData"
-            //}
-        };
+        public static IEnumerable<Control> Properties { get; set; } = new List<Control>() {  };
 
         public static Guide Guide { get; set; } = null;
         public static IntegrationType IntegrationType { get; set; } = IntegrationType.Enrichment;
