55f6a5dd245dfedae1d2561c71d884e0446bec3f
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2024-11-25 14:04:59 +0800

    feat: Add guidance note to enricher configuration page (#20)

--- src/ExternalSearch.Providers.Bregg/Constants.cs
+++ src/ExternalSearch.Providers.Bregg/Constants.cs
@@ -15,6 +15,31 @@ namespace CluedIn.ExternalSearch.Providers.Bregg
         public static string About { get; set; } = "Brreg is an enricher which provides information on Norwegian companies";
         public static string Icon { get; set; } = "Resources.brreg_logo.svg";
         public static string Domain { get; set; } = "https://www.brreg.no/";
+        public const string Instruction = """
+            [
+              {
+                "type": "bulleted-list",
+                "children": [
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the vocabulary keys to provide the input for the enricher to search for additional information. For example, if you provide the website vocabulary key for the Web enricher, it will use specific websites to look for information about companies. In some cases, vocabulary keys are not required. If you don't add them, the enricher will use default vocabulary keys."
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+            """;
 
         public static AuthMethods AuthMethods { get; set; } = new AuthMethods()
         {
@@ -71,7 +96,10 @@ namespace CluedIn.ExternalSearch.Providers.Bregg
         };
 
         public static IEnumerable<Control> Properties { get; set; } = new List<Control>() {  };
-        public static Guide Guide { get; set; } = null;
+        public static Guide Guide { get; set; } = new Guide
+        {
+            Instructions = Instruction
+        };
         public static IntegrationType IntegrationType { get; set; } = IntegrationType.Enrichment;
     }
 }
