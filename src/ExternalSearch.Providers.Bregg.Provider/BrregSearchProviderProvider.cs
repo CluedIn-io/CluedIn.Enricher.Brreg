85dd130058fa4a95d2b613da74c57f3b0be775b7
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2025-07-02 09:25:36 +0800

    feat: Supports enricherv2 (#27)

--- src/ExternalSearch.Providers.Bregg.Provider/BrregSearchProviderProvider.cs
+++ src/ExternalSearch.Providers.Bregg.Provider/BrregSearchProviderProvider.cs
@@ -10,6 +10,7 @@ using CluedIn.Core.Providers;
 using CluedIn.Core.Webhooks;
 using CluedIn.ExternalSearch;
 using CluedIn.ExternalSearch.Providers.Bregg;
+using CluedIn.ExternalSearch.Providers.Bregg.Vocabularies;
 using CluedIn.Providers.Models;
 using Constants = CluedIn.ExternalSearch.Providers.Bregg.Constants;
 
@@ -120,7 +121,24 @@ namespace CluedIn.Provider.Brreg
         public IEnumerable<Control> Properties { get; } = Constants.Properties;
         public Guide Guide { get; } = Constants.Guide;
         public new IntegrationType Type { get; } = Constants.IntegrationType;
-        public Dictionary<string, HashSet<string>> ValidRequiredFieldConfigurationCombinations => new() {
+        public bool SupportsEnricherV2 => true;
+        public Dictionary<string, object> ExtraInfo { get; } = new()
+        {
+            { "autoMap", true },
+            { "useEnricherOriginEntityCode", true },
+            { "supportConfidenceScore", false }, // for UI
+            { "minConfidenceScore", 0 }, // for UI
+            { "maxConfidenceScore", 100 }, // for UI
+            { "origin", Constants.ProviderName.ToCamelCase() },
+            { "originField", string.Empty },
+            { "nameKeyField", Constants.KeyName.NameVocabularyKey },
+            { "vocabKeyPrefix", BrregVocabulary.Organization.KeyPrefix},
+            { "autoSubmission", false },
+            { "dataSourceSetId", string.Empty },
+        };
+        
+        public Dictionary<string, HashSet<string>> ValidRequiredFieldConfigurationCombinations => new()
+        {
             {
                 "Name, Country Code and Website",
                 new HashSet<string>
